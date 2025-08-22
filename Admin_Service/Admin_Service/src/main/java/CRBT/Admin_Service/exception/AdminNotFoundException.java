95bc0714f19ca4d018f5e2a685075f171c8bf229
Fiona Noel <fionanoel775+personal@gmail.com>
2025-08-22 14:58:36 +0530

    Delete Admin_Service/Admin_Service directory

--- Admin_Service/Admin_Service/src/main/java/CRBT/Admin_Service/exception/AdminNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package CRBT.Admin_Service.exception;
-
-public class AdminNotFoundException extends RuntimeException {
-    public AdminNotFoundException(String message) {
-        super(message);
-    }
-}
