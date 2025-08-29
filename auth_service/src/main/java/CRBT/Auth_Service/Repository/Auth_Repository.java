b26a6e804dcab90d3c0db60d276074d699cdcac1
Fiona Noel <fionanoel775+personal@gmail.com>
2025-08-29 19:32:41 +0530

    Add files via upload

--- auth_service/src/main/java/CRBT/Auth_Service/Repository/Auth_Repository.java
+++ auth_service/src/main/java/CRBT/Auth_Service/Repository/Auth_Repository.java
@@ -1,6 +1,7 @@
 package CRBT.Auth_Service.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import CRBT.Auth_Service.Model.AuthUser;
 
@@ -8,6 +9,8 @@ import java.util.Optional;
 
 public interface Auth_Repository extends JpaRepository<AuthUser, Long> {
     Optional<AuthUser> findByUsername(String username);
-    Optional<AuthUser> findByUserId(Long userId);
+    
+    @Query("SELECT a FROM AuthUser a WHERE a.users_id = :users_id")
+    Optional<AuthUser> findByUsers_id(Long users_id);
     boolean existsByUsername(String username);
 }
--- auth_service/src/main/java/CRBT/Auth_Service/auth_service/AuthServiceApplication.java
+++ auth_service/src/main/java/CRBT/Auth_Service/auth_service/AuthServiceApplication.java
@@ -2,9 +2,12 @@ package CRBT.Auth_Service.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-
+@EntityScan("CRBT.Auth_Service.Model")
+@EnableJpaRepositories("CRBT.Auth_Service.Repository")  
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
