22690f9722d5cdebd90549503c44a1a23f7ac452
Fiona Noel <fionanoel775+personal@gmail.com>
2025-08-31 23:13:27 +0530

    Add files via upload

---
+++ apigateway/src/main/java/CRBT/APIGateway/apigateway/ApigatewayApplication.java
@@ -0,0 +1,13 @@
+package CRBT.APIGateway.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+}
